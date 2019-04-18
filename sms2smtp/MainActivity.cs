af2523a80b69f0be03ecf81892299f2e34e0a16b
Rijesh Augustine <rijesh@ualberta.ca>
2019-04-18 12:40:04 -0600

    First substantial commit

--- sms2smtp/MainActivity.cs
+++ sms2smtp/MainActivity.cs
@@ -1,14 +1,193 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.IO;
+using System.Net.Mail;
+using System;
+using Android.Content;
+using Android.Provider;
+using Android.Telephony;
+using Android.Net;
 
+//things to do
+//queue up unsent messages
+//have app turn on at startup
+//read sms messages
+//save password securely
 namespace sms2smtp
 {
+    [BroadcastReceiver]
+    [IntentFilter(new[] { "android.net.conn.CONNECTIVITY_CHANGE" })]
+    public class NetworkChangeReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Configuration.canSend = !intent.GetBooleanExtra(ConnectivityManager.ExtraNoConnectivity, false);
+            Console.WriteLine("NETWORK CHANGED ");
+            Console.WriteLine(Configuration.canSend);
+            Console.WriteLine("****************************************");
+        }
+    }
+
+    [BroadcastReceiver]
+    [IntentFilter(new[] { Intent.ActionBootCompleted, "android.intent.action.QUICKBOOT_POWERON" })]
+    public class BootReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            //Toast.MakeText(context, "Received intent!", ToastLength.Long).Show();
+            Intent serviceStart = new Intent(context, typeof(SmsSenderService));
+            serviceStart.AddFlags(ActivityFlags.NewTask);
+            serviceStart.SetAction("My.Action");
+            context.StartService(serviceStart);
+        }
+    }
+
+    [BroadcastReceiver(Enabled = true, Exported = true)]
+    [IntentFilter(new[] { "android.provider.Telephony.SMS_RECEIVED" })]
+    class SmsListener : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Toast.MakeText(context, "Received intent!", ToastLength.Long).Show();
+            if (Telephony.Sms.Intents.SmsReceivedAction.Equals(intent.Action))
+            {
+                foreach (SmsMessage smsMessage in Telephony.Sms.Intents.GetMessagesFromIntent(intent))
+                {
+                    String messageBody = smsMessage.MessageBody;
+                    System.Diagnostics.Debug.WriteLine(messageBody);
+                }
+            }
+            Intent serviceStart = new Intent(context, typeof(SmsSenderService));
+            serviceStart.AddFlags(ActivityFlags.NewTask);
+            serviceStart.SetAction("My.Action");
+            context.StartService(serviceStart);
+            /*
+            Intent serviceStart = new Intent(context, typeof(MainActivity));
+            serviceStart.AddFlags(ActivityFlags.NewTask);
+            context.StartActivity(serviceStart);*/
+        }
+    }
+
+    [Service(Exported = true)]
+    public class SmsSenderService : Service
+    {
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            Configuration.Initialize();
+        }
+
+        public override IBinder OnBind(Intent intent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override StartCommandResult OnStartCommand(Android.Content.Intent intent, StartCommandFlags flags, int startId)
+        {
+            // This method executes on the main thread of the application.
+            if (Configuration.isInitialized)
+            {
+                Configuration.sendTestEmail();
+            }
+
+            return StartCommandResult.Sticky;
+        }
+
+
+    }
+
+    public static class Configuration
+    {
+        public static SmtpClient SmtpServer;
+        //tannkkfussgrwhga
+        public static string host = "";
+        public static string port = "";
+        public static string password = "";
+        public static string fromemail = "";
+        public static string toemail = "";
+        public static string isSetup = "false";
+
+        public static bool canSend = true;
+        public static bool isInitialized = false;
+
+        public static bool Initialize()
+        {
+            if (!isInitialized)
+            {
+                retrievedata();
+                if (isSetup == "true")
+                {
+                    setupSMTP();
+                    isInitialized = true;
+                }
+            }
+            return isInitialized;
+        }
+
+        public static void retrievedata()
+        {
+            //retreive
+            var prefs = Application.Context.GetSharedPreferences("smtp_settings", FileCreationMode.Private);
+            host = prefs.GetString("host", "");
+            port = prefs.GetString("port", "");
+            fromemail = prefs.GetString("fromemail", "");
+            toemail = prefs.GetString("toemail", "");
+            password = prefs.GetString("password", "");
+            isSetup = prefs.GetString("isSetup", "false");
+        }
+
+        public static void saveprefs()
+        {
+            //store
+            isSetup = "true";
+            var prefs = Application.Context.GetSharedPreferences("smtp_settings", FileCreationMode.Private);
+            var prefEditor = prefs.Edit();
+            prefEditor.PutString("host", host);
+            prefEditor.PutString("port", port);
+            prefEditor.PutString("fromemail", fromemail);
+            prefEditor.PutString("toemail", toemail);
+            prefEditor.PutString("password", password);
+            prefEditor.PutString("isSetup", isSetup);
+
+            prefEditor.Commit();
+        }
+
+        public static void setupSMTP()
+        {
+            SmtpServer = new SmtpClient(host);
+            SmtpServer.Port = Convert.ToInt32(port);
+            SmtpServer.Host = host;
+            SmtpServer.EnableSsl = true;
+            SmtpServer.UseDefaultCredentials = false;
+            SmtpServer.Credentials = new System.Net.NetworkCredential(fromemail, password);
+        }
+
+        public static void sendTestEmail()
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+
+                mail.From = new MailAddress(fromemail);
+                mail.To.Add(toemail);
+                mail.Subject = "sms2smtp test email";
+                mail.Body = "Successful test email";
+
+                SmtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                //System.Diagnostics.Debug.WriteLine("---------###----------------");
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+        }
+    }
+
     [Activity(Label = "sms2smtp", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -19,8 +198,38 @@ namespace sms2smtp
             // Get our button from the layout resource,
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.myButton);
+            EditText host_txt = FindViewById<EditText>(Resource.Id.smtp_host);
+            EditText port_txt = FindViewById<EditText>(Resource.Id.smtp_port);
+            EditText email_from_txt = FindViewById<EditText>(Resource.Id.smtp_email);
+            EditText password_txt = FindViewById<EditText>(Resource.Id.smtp_password);
+            EditText email_to_field_txt = FindViewById<EditText>(Resource.Id.email_to_field);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            TextView configured_box = FindViewById<TextView>(Resource.Id.configured_box);
+
+            Configuration.Initialize();
+            if (Configuration.isInitialized)
+            {
+                host_txt.Text = Configuration.host;
+                port_txt.Text = Configuration.port;
+                password_txt.Text = Configuration.password;
+                email_from_txt.Text = Configuration.fromemail;
+                email_to_field_txt.Text = Configuration.toemail;
+
+                configured_box.Text = "CONFIGURED";
+            }
+
+            button.Click += delegate {
+                Configuration.host = host_txt.Text;
+                Configuration.port = port_txt.Text;
+                Configuration.password = password_txt.Text;
+                Configuration.fromemail = email_from_txt.Text;
+                Configuration.toemail = email_to_field_txt.Text;
+                Configuration.setupSMTP();
+                Configuration.saveprefs();
+                Configuration.sendTestEmail();
+
+                configured_box.Text = "CONFIGURED";
+            };
         }
     }
 }
--- sms2smtp/Resources/Resource.Designer.cs
+++ sms2smtp/Resources/Resource.Designer.cs
@@ -44,8 +44,50 @@ namespace sms2smtp
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f05000b
+			public const int configured_box = 2131034123;
+			
+			// aapt resource value: 0x7f050009
+			public const int email_to_field = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int myButton = 2131034122;
+			
+			// aapt resource value: 0x7f050005
+			public const int smtp_email = 2131034117;
+			
+			// aapt resource value: 0x7f050001
+			public const int smtp_host = 2131034113;
+			
+			// aapt resource value: 0x7f050007
+			public const int smtp_password = 2131034119;
+			
+			// aapt resource value: 0x7f050003
+			public const int smtp_port = 2131034115;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView2 = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView3 = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int textView4 = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int textView5 = 2131034120;
+			
+			// aapt resource value: 0x7f05000d
+			public const int textView7 = 2131034125;
+			
+			// aapt resource value: 0x7f05000e
+			public const int textView8 = 2131034126;
+			
+			// aapt resource value: 0x7f05000c
+			public const int textView9 = 2131034124;
 			
 			static Id()
 			{
